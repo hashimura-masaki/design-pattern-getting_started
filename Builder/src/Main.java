d5e4b3deb2453f753e17bcae7f5d716a2a208fe8
hashimura-masaki <motimoti0730@gmail.com>
2019-07-15 21:13:52 +0900

    Builderパターン作成

---
+++ Builder/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		TextBuilder textBuilder = new TextBuilder();
+		Director director = new Director(textBuilder);
+		director.construct();
+		String result = textBuilder.getResult();
+		System.out.println(result);
+	}
+
+}
